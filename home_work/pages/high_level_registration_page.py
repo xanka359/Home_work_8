5eefa15ede43b42ac52c9b2baa5c13c1effd95a5
Oksana Bondareva <xanka359@yandex.ru>
2024-03-04 00:23:52 +0300

    commit for the second branch (refactor order of functions)

--- home_work/pages/high_level_registration_page.py
+++ home_work/pages/high_level_registration_page.py
@@ -18,26 +18,13 @@ class HighLevelRegistrationPage:
     def open(self):
         browser.open('/')
         return self
+
     def type_first_name(self, f_name):
         self.first_name.should(be.blank).type(f_name)
 
     def type_last_name(self, l_name):
         self.last_name.should(be.blank).type(l_name)
         return self
-
-    def date_of_birth(self, year, month, day):
-        browser.element('#dateOfBirthInput').click()
-        browser.element(".react-datepicker__month-select").click().element(by.text(month)).click()
-        browser.element(".react-datepicker__year-select").click().element(
-            by.text(year)).click()
-        browser.element(f'.react-datepicker__day--0{day}').click()
-
-    def should_registrated_user_contains(self, *args):
-        return browser.element('.table').all('td').even.should(have.texts(args))
-
-    def fill_state(self, state):
-        self.state.element('.css-19bqh2r').click()
-        browser.element('#react-select-3-option-0').should(have.text(state)).click()
 
     def type_email(self, email):
         self.email.should(be.blank).type(email)
@@ -48,6 +35,13 @@ class HighLevelRegistrationPage:
 
     def type_phone_number(self, ph_number):
         self.p_number.should(be.blank).type(ph_number)
+
+    def date_of_birth(self, year, month, day):
+        browser.element('#dateOfBirthInput').click()
+        browser.element(".react-datepicker__month-select").click().element(by.text(month)).click()
+        browser.element(".react-datepicker__year-select").click().element(
+            by.text(year)).click()
+        browser.element(f'.react-datepicker__day--0{day}').click()
 
     def select_subject(self, subject):
         browser.element('#subjectsInput').type(subject).press_enter()
@@ -62,6 +56,10 @@ class HighLevelRegistrationPage:
     def type_current_address(self, address):
         self.current_address.should(be.blank).type(address)
 
+    def fill_state(self, state):
+        self.state.element('.css-19bqh2r').click()
+        browser.element('#react-select-3-option-0').should(have.text(state)).click()
+
     def fill_city(self, city):
         self.city.element('.css-1wy0on6').click()
         browser.element('#react-select-4-option-2').should(have.text(city)).click()
@@ -72,6 +70,9 @@ class HighLevelRegistrationPage:
 
     def modal_registration_text(self, pop_up_text):
         browser.element('#example-modal-sizes-title-lg').should(have.text(pop_up_text))
+
+    def should_registrated_user_contains(self, *args):
+        return browser.element('.table').all('td').even.should(have.texts(args))
 
     def close_modal_win(self):
         browser.element('#closeLargeModal').click()
--- tests/test_high_level_registration_page.py
+++ tests/test_high_level_registration_page.py
@@ -1,17 +1,18 @@
 from home_work.pages.high_level_registration_page import HighLevelRegistrationPage
 from home_work.users import User
 
-
-user = User(first_name='NewFirst name', last_name='NewLast name', date_of_birth= 18, month_of_birth= 'June',
-            year_of_birth= '1995', email='example@mail.com', gender='Female', phone_number=1023456789,
+user = User(first_name='NewFirst name', last_name='NewLast name', date_of_birth=18, month_of_birth='June',
+            year_of_birth='1995', email='example@mail.com', gender='Female', phone_number=1023456789,
             current_address='Russian Federation, Penza', city='Noida', state='NCR')
 
-user_interests = User.UserInterests(subject = 'English', hobby = 'Sports')
+user_interests = User.UserInterests(subject='English', hobby='Sports')
+
+
 def test_registrated_user():
     registration_page = HighLevelRegistrationPage()
 
     registration_page.open()
-
+    #THEN
     registration_page.type_first_name(user.first_name)
     registration_page.type_last_name(user.last_name)
 
@@ -30,12 +31,12 @@ def test_registrated_user():
     registration_page.fill_city(user.city)
 
     registration_page.submit()
-
+    #WHEN
     registration_page.modal_registration_text('Thanks for submitting the form')
     registration_page.should_registrated_user_contains(f'{user.first_name} {user.last_name}', user.email,
                                                        user.gender, f'{user.phone_number}',
                                                        f'{user.date_of_birth} {user.month_of_birth},{user.year_of_birth}',
                                                        user_interests.subject, user_interests.hobby,
                                                        'photo_1.jpg', user.current_address, f'{user.state} {user.city}'
-                   )
+                                                       )
     registration_page.close_modal_win()
