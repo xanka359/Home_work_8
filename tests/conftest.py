85eb5dfc7e076a1ee73c95cccf729fb6ed751ef1
xanka359 <159315307+xanka359@users.noreply.github.com>
2024-03-04 00:10:43 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,11 @@ from selene import browser
 import pytest
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def manage_browser():
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.config.window_height = 1080  # задает высоту окна браузера
     browser.config.window_width = 1920
-    #browser.config.timeout = 6.0
 
     yield
     browser.quit()
