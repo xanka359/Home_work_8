d4789115efb015e48b7a988d45df24b8b0219191
Oksana Bondareva <xanka359@yandex.ru>
2024-03-10 00:01:41 +0300

    fourth commit for the HL branch with changes in requirements,attach, conft, test_student...

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,34 @@
-from selene import browser
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 
+from utils import attach
 
-@pytest.fixture(scope='session', autouse=True)
-def manage_browser():
+@pytest.fixture(scope='function')
+def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
-
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
-    #browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    #browser.config.window_height = 1080  # задает высоту окна браузера
-    #browser.config.window_width = 1920
     browser = Browser(Config(driver))
-
     yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
--- tests/test_student_registration_form.py
+++ tests/test_student_registration_form.py
@@ -1,3 +1,5 @@
+import allure
+
 from demo_qa.pages.student_registration_form import StudentRegistrationForm
 from demo_qa.users import user, user_interests
 
@@ -5,6 +7,11 @@ from demo_qa.users import user, user_interests
 def test_registrated_user():
     registration_page = StudentRegistrationForm()
 
-    registration_page.open()
-    registration_page.registration(user)
-    registration_page.modal_window_check(user, user_interests)
+    with allure.step("Открыть форму решистрации"):
+        registration_page.open()
+
+    with allure.step("Заполнить данные в поля формы регистрации"):
+        registration_page.registration(user)
+
+    with allure.step("Проверить корректность заполнения полей формы"):
+        registration_page.modal_window_check(user, user_interests)
