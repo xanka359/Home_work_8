d1e0d5b5d06b71aa2e0a160ad0384eb30b6a5365
Oksana Bondareva <xanka359@yandex.ru>
2024-03-13 23:53:38 +0300

    commit for the HL branch with new changes for HW_11

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,19 @@
-import pytest
+import os
 
+import pytest
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 from utils import attach
 
+
+@pytest.fixture( autouse=True)
+def load_env():
+    load_dotenv()
+
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -18,8 +24,12 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
@@ -27,9 +37,6 @@ def setup_browser(request):
     # Масштабирование страницы
     browser.driver.set_window_size(1920, 1080)  # Устанавливаем размер окна браузера
     browser.driver.set_window_position(0, 0)  # Устанавливаем позицию окна браузера на экране
-
-    # Прокрутка страницы
-    #browser.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
     yield browser
 
